c0db457426f39b845d7a9c9462eb188e3f9c6eb3
Alex Liu <gloria_domini@hotmail.com>
2019-08-06 21:20:46 +1000

    A

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Bullet : MonoBehaviour
+
+    
+{
+
+    [Header("Attributes")]
+    public int damage;
+    public float speed;
+    private Rigidbody2D rigid;
+    private MasterController masterController;
+    public Transform playerPos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPos = GameObject.Find("Player").transform;
+        rigid = GetComponent<Rigidbody2D>();
+        masterController = GetComponent<MasterController>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Fire()
+    {
+        rigid.velocity = (playerPos.transform.up).normalized * speed;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Enemy enemy = collision.GetComponent<Enemy>();
+        if(enemy)
+        {
+            //enemy.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [Header("References")]
+    public MasterController attackJoystick;
+    [Header("Attributes")]
+    public float rotateSpeed;
+
+    private float shootTimer;
+    public float shootRate = 2f;
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
+
+    private void Start()
+    {
+        
+       // firePoint = GameObject.Find("FirePoint").transform;
+    }
+    void Update()
+    {
+
+        Rotate();
+        if (Time.time > shootTimer)
+        {
+            shootTimer = Time.time + shootRate;
+            Shoot();
+        }
+
+    }
+    public void Shoot()
+    {
+
+        GameObject bulletGo = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+        Bullet bullet = bulletGo.GetComponent<Bullet>();
+        bullet.Fire();
+        Destroy(bulletGo, 1f);
+
+        
+
+    }
+    public void Rotate()
+    {
+        transform.Rotate(new Vector3(0, 0, attackJoystick.Horizontal), rotateSpeed);
+    }
+}
+
+
+
