a4c2dfe2c091435135693f717e1010a53d424fed
irlartboy <ollie_king99@outlook.com>
2019-08-14 09:08:30 +1000

    Camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject follow;
+
+    public float speed = 2f;
+
+    private void Update()
+    {
+        float interperlation = speed * Time.deltaTime;
+
+        Vector3 position = this.transform.position;
+        position.y = Mathf.Lerp(this.transform.position.y, follow.transform.position.y, interperlation);
+        position.x = Mathf.Lerp(this.transform.position.x, follow.transform.position.x, interperlation);
+
+        this.transform.position = position;
+
+    }
+
+}
