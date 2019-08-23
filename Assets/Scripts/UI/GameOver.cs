2e0d22880c4da6d0958fac04fbd81286d9b6bb82
irlartboy <ollie_king99@outlook.com>
2019-08-23 23:59:25 +1000

    updates

---
+++ Assets/Scripts/UI/GameOver.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameOver : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 1);
+    }
+    public void ExitButton()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+}
